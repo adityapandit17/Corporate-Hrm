eeda1124d60f20ff6fea14a20d429005d940bd66
ADITYA <adityapandit17>
2018-05-11 02:17:54 +0530

    initial commit

---
+++ app/models/employee.rb
@@ -0,0 +1,17 @@
+class Employee < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  ROLES = %w[admin  employee].freeze
+
+  def admin?
+  	self.role == "admin"
+  end
+
+  def employee?
+  	self.role == "employee"
+  end
+
+end
